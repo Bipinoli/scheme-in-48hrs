ea0298890a5e5c8222cface46bc07cd4d01d43af
boli <bipinoli90@gmail.com>
2024-07-07 18:13:18 +0200

    parsing

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## Scheme in 48 hrs
+
+Following the [Write yourself a scheme in 48 hours](https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours) series of articles to practice Haskell
---
+++ app/Main.hs
@@ -0,0 +1,52 @@
+module Main where
+
+import Control.Monad (liftM)
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+  _ <- char '"'
+  str <- many (noneOf "\"")
+  _ <- char '"'
+  return $ String str
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "Main.hs" input of
+  Left err -> "No match: " ++ show err
+  Right _ -> "Found value"
+
+main :: IO ()
+main = do
+  (expr : _) <- getArgs
+  putStrLn (readExpr expr)
